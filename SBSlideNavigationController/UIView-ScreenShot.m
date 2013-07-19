5abeaa466f136356225bd495b0678e4359b20990
Alberto Petrucci <capriele93@hotmail.it>
2013-07-20 00:24:22 +0200

    Update UIView-ScreenShot.m
    speed improvements

--- SBSlideNavigationController/UIView-ScreenShot.m
+++ SBSlideNavigationController/UIView-ScreenShot.m
@@ -12,18 +12,32 @@
 
 @implementation UIView (ScreenShot)
 
+CGImageRef UIGetScreenImage(void);
+
 + (UIImage *)screenShotForView:(UIView *)view
 {
   return [view screenShot];
 }
 
-- (UIImage *)screenShot
-{
-  UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0.0);
-  [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  return image;
+- (UIImage *)screenShot {
+    float statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height,
+          navBarHeight = self.firstAvailableUIViewController.navigationController.navigationBar.frame.size.height,
+          sum = statusBarHeight+navBarHeight;
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(displayLinkWithTarget:selector:)] &&
+        ([UIScreen mainScreen].scale == 2.0)) {
+        // Retina display
+        sum *= 2;
+    } 
+    
+    CGImageRef screen = UIGetScreenImage();
+    UIImage *image = [UIImage imageWithCGImage:screen];
+    CGImageRelease(screen);
+    
+    CGRect rect = CGRectMake(0, sum, image.size.width, image.size.height-sum);
+    CGImageRef imageRef = CGImageCreateWithImageInRect([image CGImage], rect);
+    image = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    return image;  
 }
 
 @end
